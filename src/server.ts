81a6e046479c236d52c74c43c0706d2493f0b325
tahaamin2013 <tahaamindob2013@gmail.com>
2024-01-01 15:16:36 +0500

    Hey

--- src/server.ts
+++ src/server.ts
@@ -15,10 +15,8 @@ const start = async () => {
     },
   })
   app.use((req, res) => nextHandler(req, res))
-
   nextApp.prepare().then(() => {
     // payload.logger.info('Next.js started')
-
     app.listen(PORT, async () => {
       // payload.logger.info(
       //   `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
